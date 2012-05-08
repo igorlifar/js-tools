e7548568d1f9dd78be954e9c3b517c69e5881ea6
Igor Lifar <igorlifar@omnihead.ru>
2012-05-08 19:01:31 +0400

    implementation

--- documentFilter/documentFilter.js
+++ documentFilter/documentFilter.js
@@ -1,57 +1,117 @@
-/*
-document structure:
-{
-	id: <id>,
-	fields: {
-		<key1>: <value1>,
-		<key2>: <value2>,
-		...
-		<keyn>: <valuen>
+
+var documentFilter = function(documents){
+	this.documents = documents;
+};
+
+var cmpE = function(a, b){
+	if(a.x != b.x){
+		return parseInt(a.x) < parseInt(b.x);
+	}
+	return parseInt(a.t) < parseInt(b.t);
+};
+
+var sort = function(events, l, r){
+	l = parseInt(l);
+	r = parseInt(r);
+	if(l >= r){
+		return;
+	}
+	var mid = Math.floor((l + r) / 2);
+	sort(events, l, mid);
+	sort(events, mid + 1, r);
+	var k = 0;
+	var e = new Array();
+	var i = l;
+	var j = mid + 1;
+	while(i <= mid && j <= r){
+		if(cmpE(events[i], events[j])){
+			e[k++] = events[i++];
+		}
+		else{
+			e[k++] = events[j++];
+		}
+	}
+	while(i <= mid){
+		e[k++] = events[i++];
+	}
+	while(j <= r){
+		e[k++] = events[j++];
+	}
+	for(var i = l; i <= r; i++){
+		events[i] = e[i - l];
 	}
 }
 
-example:
-
-{
-	id: 3,
-	fields: {
-		title: "Title of the article",
-		body: "foo foo foo foo",
-		author: "jack gordon"
+documentFilter.prototype.filter = function(query){
+	var words = new Array();
+	var currentString = "";
+	query += " ";
+	var size = 0;
+	for(var i = 0; i < query.length; i++){
+		if(query[i] == " "){
+			if(currentString.length > 0){
+				words[size++] = currentString.toUpperCase();
+			}
+			currentString = "";
+		}
+		else{
+			currentString += query[i];
+		}
 	}
-}
-
-documents = [<document1>, <document2>, ... <documentN>]
-
-*/
-
-var documentFilter = function(documents) {
-
-}
-
-
-
-documentFilter.prototype.filter = function(query) {
-
-}
-
-/* result structure:
-
-[<document1>, ... <documentn>]
-
-document structure:
-
-{
-	id: <id>,
-	fields: {
-		<key1>: [<segment1>, <segment2>, ... <segmentN>],
-		...
+	var size = 0;
+	var documents = new Array();
+	for(var i = 0; i < this.documents.length; i++){
+		var fields = {};
+		var good = false;
+		for(key in this.documents[i].fields){
+			var k = 0;
+			var events = new Array();
+			for(var j = 0; j < words.length; j++){
+				var start = this.documents[i].fields[key].toUpperCase().indexOf(words[j]);
+				if(start != -1){
+					events[k++] = {"x" : start, "t" : 1};
+					events[k++] = {"x" : start + words[j].length - 1, "t" : 2};
+				}
+			}
+			if(k == 0){
+				continue;
+			}
+			sort(events, 0, k - 1);
+			var n = 0;
+			var currentSegments = new Array();
+			var lastx = -1;
+			var balance = 0;
+			for(var j = 0; j < k; j++){
+				if(events[j].t == 1){
+					if(balance == 0){
+						lastx = events[j].x;
+					}
+					balance++;
+				}
+				else{
+					balance--;
+					if(balance == 0){
+						currentSegments[n++] = {"begin" : lastx, "end" : events[j].x};
+					}
+				}
+			}
+			var m = 0;
+			var segments = new Array();
+			var j = 0;
+			while(j < currentSegments.length){
+				var z = j;
+				while(z + 1 < currentSegments.length && currentSegments[z].end + 1 == currentSegments[z + 1].begin){
+					z++;
+				}
+				segments[m++] = {"begin" : currentSegments[j].begin, "end" : currentSegments[z].end};
+				j = z + 1;
+			}
+			fields[key] = segments;
+			good = true;
+		}
+		if(good){
+			documents[size++] = {"id" : this.documents[i].id, "fields" : fields};
+		}
 	}
-}
-
-segment: {
-	begin: <begin index>,
-	end: <end index>
-}
-
-*/
+	return documents;
+};
